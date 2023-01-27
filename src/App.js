9834f2efc438f041c8c003482097f5caab90c1bc
Vilho Teivainen <vilho.teivainen@ambientia.fi>
2023-01-27 14:59:00 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Ave Mundi!
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
